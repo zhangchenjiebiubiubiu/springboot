b954e057499aa73480223c86de431cc81a658c22
zhangchenjie <1617066383@qq.com>
2020-04-11 15:10:49 +0800

    springboot 学习代码

---
+++ src/main/java/it/gm/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package it.gm.springboot;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("it.gm.springboot.dao")
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
